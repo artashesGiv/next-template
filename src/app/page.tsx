8ffe73a74eccb69739eb9b1c87e116187cbb4350
dzhivanyan <dzhivanyan>
2025-02-06 16:12:27 +0300

    add zustand

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,28 @@
+'use client'
+
 import './page.scss'
-import { IconBase } from '@/shared'
+import { Button, IconBase } from '@/shared'
+import { create } from 'zustand'
+
+interface Store {
+  count: number
+  increment: () => void
+}
+
+const useStore = create<Store>(set => ({
+  count: 0,
+  increment: () => set(state => ({ count: state.count + 1 })),
+}))
 
 export default function Home() {
+  const { count, increment } = useStore()
+
   return (
     <div className='home'>
-      <h1>HOME</h1>
-      <IconBase name='cross' />
+      <h1>
+        {<IconBase name='cross' />} HOME {count}
+      </h1>
+      <Button onClick={increment}>increment</Button>
     </div>
   )
 }
